2334633aedc50332a39c096a07f5b55b9e78a0ab
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-09-14 13:29:00 -0400

    blog

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+const BlogPage = () => {
+    return (
+      <main>
+        <title>Haidy's Blog</title>
+        <h1>Welcome to my blog!</h1>
+        <p></p>
+      </main>
+    )
+  }
+  // Step 3: Export your component
+  export default BlogPage
