22d625617677afdb32bc139298a3c008c01a7f05
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-09-14 13:30:19 -0400

    react

---
+++ src/pages/react.js
@@ -0,0 +1,11 @@
+const ReactPage = () => {
+    return (
+      <main>
+        <title>React/Redux Project</title>
+        <h1>World Hopper!</h1>
+        <p></p>
+      </main>
+    )
+  }
+  // Step 3: Export your component
+  export default ReactPage
